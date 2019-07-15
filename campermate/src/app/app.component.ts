e68ded18598db53fa4530f389a36c22c4753a4d6
Prahalad Belavadi <prahalad@belavadi.com>
2019-07-15 23:36:27 +0530

    import assets, cap sync and install cordova sql lite
    Signed-off-by: Prahalad Belavadi <prahalad@belavadi.com>

--- campermate/src/app/app.component.ts
+++ campermate/src/app/app.component.ts
@@ -1,26 +1,21 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins } from "@capacitor/core";
+const { SplashScreen, StatusBar } = Plugins;
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  constructor(
-    private platform: Platform,
-    private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
-    this.initializeApp();
-  }
+  constructor() {
+    SplashScreen.hide().catch((err) => {
+      console.warn(err);
+    });
 
-  initializeApp() {
-    this.platform.ready().then(() => {
-      this.statusBar.styleDefault();
-      this.splashScreen.hide();
+    StatusBar.hide().catch((err) => {
+      console.warn(err);
     });
   }
-}
+};
